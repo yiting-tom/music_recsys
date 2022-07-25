31aee5ae577a954c7c938174489b226955d2fdbc
Yi-Ting Li <liyitingtom@gmail.com>
2022-07-25 16:01:05 +0800

    feat: track spider
    - parsing for ram htm to tarck lyrics
    - get all track ids from csv file into iterable

--- lyrics_scrapy/lyrics_scrapy/spiders/track.py
+++ lyrics_scrapy/lyrics_scrapy/spiders/track.py
@@ -4,7 +4,7 @@ import scrapy
 class TrackSpider(scrapy.Spider):
     name = 'track'
     allowed_domains = ['mojim.com']
-    start_urls = 
+    start_urls = []
 
     def parse(self, response):
         pass
